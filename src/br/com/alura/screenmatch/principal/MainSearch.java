f6f2cd3cded8af5f1ec3d3912db8fc51e811576a
Kevin Costa <midas159@outlook.com>
2026-01-13 01:00:46 -0300

    modificando MainSearch

--- src/br/com/alura/screenmatch/principal/MainSearch.java
+++ src/br/com/alura/screenmatch/principal/MainSearch.java
@@ -1,6 +1,26 @@
 package br.com.alura.screenmatch.principal;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class MainSearch {
-    static void main() {
+    static void main() throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o nome do filme para busca:");
+        var busca = leitura.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=996e7daa";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
