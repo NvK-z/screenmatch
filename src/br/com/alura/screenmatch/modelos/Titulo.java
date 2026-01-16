6946079d09180e55441370a264de29d722195571
Kevin Costa <midas159@outlook.com>
2026-01-15 23:35:38 -0300

    mudanças no MainSearch

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -11,6 +15,17 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb tituloOmdb) {
+        this.nome = tituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(tituloOmdb.year());
+        String runtime = tituloOmdb.runtime();
+        if (runtime != null && runtime.endsWith(" min")) {
+            this.duracaoEmMinutos = Integer.valueOf(runtime.replace(" min", ""));
+        } else {
+            this.duracaoEmMinutos = 0;
+        }
     }
 
     public String getNome() {
@@ -66,5 +81,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + "\n" +
+                "Ano de Lançamento: " + anoDeLancamento + "\n" +
+                "Tempo de Duração: " + duracaoEmMinutos + " minutos";
     }
 }
--- src/br/com/alura/screenmatch/principal/MainSearch.java
+++ src/br/com/alura/screenmatch/principal/MainSearch.java
@@ -1,5 +1,11 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -15,12 +21,32 @@ public class MainSearch {
 
         String endereco = "http://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=996e7daa";
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+
+            TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(tituloOmdb);
+            Titulo meuTitulo = new Titulo(tituloOmdb);
+            System.out.println("Titulo convertido e Ano de Lançamento: ");
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e) {
+            System.out.println("Ocorreu um erro ao converter o ano de lançamento do filme: " + e.getMessage());
+            System.out.println("Verifique se os dados fornecidos estão corretos.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ocorreu um erro ao processar a requisição: " + e.getMessage());
+            System.out.println("Verifique se o nome do filme foi digitado corretamente.");
+        } finally {
+            System.out.println("Busca finalizada.");
+        }
     }
 }
